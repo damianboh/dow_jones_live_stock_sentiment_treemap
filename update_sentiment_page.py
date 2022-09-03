0613fffd093978232d0ba70338cff2f71e135cde
Damian Boh <bck1990@gmail.com>
2022-09-03 22:23:14 +0800

    Update update_sentiment_page.py

--- update_sentiment_page.py
+++ update_sentiment_page.py
@@ -71,8 +71,8 @@ for file_name, news_table in news_tables.items():
 
 			# Append ticker, date, time and headline as a list to the 'parsed_news' list
 			parsed_news.append([ticker, date, time, text])
-		except:
-			None
+		except Exception as e:
+			print(e)
 
 # Perform Sentiment Analysis with Vader
 # Instantiate the sentiment intensity analyzer
